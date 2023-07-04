1e50a6c3ea4838585a30eceeea70a4956362d490
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2023-07-04 16:28:24 +0700

    Добавьте файлы проекта.

---
+++ MatrixHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDPDZ
+{
+    internal class MatrixHelper
+    {
+        public static Random r = new Random();
+
+        public static bool Equals(int[][] m1, int[][] m2)
+        {
+            if (m1.Length != m2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < m1.Length; i++)
+            {
+                if (m1[i].Length != m2[i].Length)
+                {
+                    return false;
+                }
+                for (int j = 0; j < m1[i].Length; j++)
+                {
+                    if (m1[i][j] != m2[i][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static void DisplayMatrix(int[][] matrix)
+        {
+            foreach (var row in matrix)
+            {
+                foreach (var item in row)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static int[][] GenerateMatrix(int m, int n, int min, int max)
+        {
+            try
+            {
+                int[][] matrix = new int[m][];
+                for (int i = 0; i < m; i++)
+                {
+                    matrix[i] = new int[n];
+                }
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i][j] = r.Next(min, max + 1);
+                    }
+                }
+                return matrix;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception occurred: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDPDZ
+{
+    internal class Program
+    {
+        public static int[][] ProizMatrix(int[][] m1, int[][] m2, ref int[][] res, int skolar)
+        {
+            for (int i = 0; i < m1.Length; i++)
+            {
+                for (int j = 0; j < m1.Length; j++)
+                {
+                    m2[i][j] *= skolar;
+                }
+            }
+            res = new int[m1.Length][];
+            for(int i  = 0; i < m1.Length; i++)
+            {
+                res[i] = new int[m1[i].Length];
+            }
+            if (m1.Length != m2.Length)
+            {
+                return null;
+            }
+            else
+            {
+                for (int i = 0; i < m1.Length; i++)
+                {
+                    for (int j = 0; j < m1.Length; j++)
+                    {
+                        res[i][j] = m1[i][j]*m2[j][i];
+                    }
+                }
+                return res;
+            }
+        }
+
+        public static void PartProiz(int[][] m1,int[][] m2, int start, int end, ref int[][] res ,int skolar)
+        {
+            for (int i = start; i < end; i++)
+            {
+                for (int j = 0; j < m1.Length; j++)
+                {
+                    m2[i][j] *= skolar;
+                }
+            }
+            try
+            {
+                for (int i = start; i < end; i++)
+                {
+                    for (int j = 0; j < m1.Length; j++)
+                    {
+                        res[i][j] = m1[i][j] * m2[j][i];
+                    }
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+        }
+
+        public static int[][] MultiThread(int[][]m1, int[][]m2, int threadNum, int skolar)
+        {
+            int[][] res = new int[m1.Length][];
+            for (int i = 0; i < m1.Length; i++)
+            {
+                res[i] = new int[m1[i].Length];
+            }
+            int rowPerThread = m1.Length/threadNum;
+            Thread[] threads = new Thread[threadNum];
+            for(int i = 0; i < threadNum; i++)
+            {
+                int start = i * rowPerThread;
+                int end = start + rowPerThread;
+                if( i == threadNum -1)
+                {
+                    end = m1.Length;
+                }
+                threads[i] = new Thread(() => PartProiz(m1,m2, start, end, ref res, skolar));
+                threads[i].Start();
+                
+               
+            }
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            int mnoj = -1;
+            int N = 10000;
+            var m1 = MatrixHelper.GenerateMatrix(N, N, 1, 10);
+            var m2 = MatrixHelper.GenerateMatrix(N, N, 1, 10);
+            int[][] res = null;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ProizMatrix(m1, m2, ref res, mnoj);
+            Console.WriteLine("(Одно поточный)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+            //MatrixHelper.DisplayMatrix(res);
+           
+            sw.Stop();
+            sw.Restart();
+            var res2 = MultiThread(m1 , m2, 2, mnoj);
+            Console.WriteLine("(Многопоточьный 2 потока)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+            //MatrixHelper.DisplayMatrix(res2);
+
+            sw.Stop();
+            sw.Restart();
+            var res3 = MultiThread(m1, m2, 4, mnoj);
+            Console.WriteLine("(Многопоточьный 4 потока)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+
+            sw.Stop();
+            sw.Restart();
+            var res4 = MultiThread(m1, m2, 8, mnoj);
+            Console.WriteLine("(Многопоточьный 8 потока)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+
+            sw.Stop();
+            sw.Restart();
+            var res5 = MultiThread(m1, m2, 16, mnoj);
+            Console.WriteLine("(Многопоточьный 16 потока)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+
+            sw.Stop();
+            sw.Restart();
+            var res6 = MultiThread(m1, m2, 32, mnoj);
+            Console.WriteLine("(Многопоточьный 32 потока)Затраченое время в милисекундах : " + sw.ElapsedMilliseconds + "\n");
+        }
+    }
+}
